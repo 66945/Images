992d57313690e357650f118741f8e2e59d85bf6f
66945 <dtmcgra@gmail.com>
2023-01-11 09:46:05 -0700

    added the stuff

--- Images/ViewController.swift
+++ Images/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet
+    weak var myImageView: UIImageView!
+    
+    let images = [
+        UIImage(named: "Cows"),
+        UIImage(named: "Image"),
+        UIImage(named: "Cow2"),
+    ]
+    var imageNum = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myImageView.image = images[0]
     }
-
-
+    
+    @IBAction
+    func onClick(_ sender: UIButton) {
+        imageNum += 1
+        imageNum %= images.count
+        
+        myImageView.image = images[imageNum]
+    }
 }
-
